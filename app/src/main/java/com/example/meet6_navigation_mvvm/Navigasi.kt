9206034ba9ac8e5bce492a6b917d8060c10c05ba
sekarasriii <sekareeesh@gmail.com>
2025-11-06 17:00:10 +0700

    menambahkan tampildata di backbuttonclick

--- app/src/main/java/com/example/meet6_navigation_mvvm/Navigasi.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/Navigasi.kt
@@ -29,6 +29,13 @@ fun DataApp(
                     }
                 )
             }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
     }
     }
 }
