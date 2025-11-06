55fe58fc18cf85bf1b6ad2cac64743a54da1776c
sekarasriii <sekareeesh@gmail.com>
2025-11-06 15:34:38 +0700

    menambahkan button click untuk pilihan jenis kelamin

--- app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
@@ -1,6 +1,10 @@
 package com.example.meet6_navigation_mvvm.view
 
+import android.R
 import androidx.compose.runtime.Composable
 
 @Composable
-fun FormIsian()
+fun FormIsian(
+    jenisK:List<R.string> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+)
