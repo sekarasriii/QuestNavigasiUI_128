42dd6a681600f48b69b1ce601d9041141491bf42
sekarasriii <sekareeesh@gmail.com>
2025-11-06 16:04:25 +0700

    menambahkan horizontal divider kedua

--- app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
@@ -63,8 +63,15 @@ fun FormIsian(
                             selected = false,
                             onClick = {item}
                         )
+                        Text(text = item)
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
         }
     }
