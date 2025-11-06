b15ee83cd8970b230ce8f4fb2d38f311063d5de9
sekarasriii <sekareeesh@gmail.com>
2025-11-06 15:35:31 +0700

    menambahkan scaffold tampilan layar

--- app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
@@ -1,10 +1,14 @@
 package com.example.meet6_navigation_mvvm.view
 
 import android.R
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
     jenisK:List<R.string> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick : () -> Unit
-)
+) {
+    Scaffold(modifier = Modifier)
+}
