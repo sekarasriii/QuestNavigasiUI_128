54878eaa0332ea6624172bbbb12901c83068b991
sekarasriii <sekareeesh@gmail.com>
2025-11-06 16:52:48 +0700

    menambahkan function DataApp

--- app/src/main/java/com/example/meet6_navigation_mvvm/Navigasi.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/Navigasi.kt
@@ -1,6 +1,17 @@
 package com.example.meet6_navigation_mvvm
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavControlle
+
 enum class Navigasi{
     Formulir,
     Detail
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){}
--- app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
@@ -1,36 +1,34 @@
 package com.example.meet6_navigation_mvvm.view
 
-import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import android.R.attr.title
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.material3.TopAppBar
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.DividerDefaults.Thickness
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.RadioButton
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.foundation.layout.fillMaxWidth
+import com.example.meet6_navigation_mvvm.R
 
 @Composable
 fun FormIsian(
-    jenisK:List<R.string> = listOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick : () -> Unit
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    onSubmitBtnClick: () -> Unit
 ) {
     Scaffold(
         modifier = Modifier,
@@ -38,12 +36,13 @@ fun FormIsian(
             TopAppBar(
                 title = {
                     Text(
-                        stringResource(R.string.home),
+                        text = stringResource(R.string.home),
                         color = Color.White
                     )
                 },
-                colors = TopAppBarDefaults.topAppBarColors
-                    (colorResource(id = R.color.teal_700))
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
             )
         }
     ) { isiRuang ->
@@ -59,25 +58,9 @@ fun FormIsian(
                     .padding(top = 20.dp)
                     .width(250.dp),
                 label = { Text(text = "Nama Lengkap") },
-                onValueChange = {},
+                onValueChange = {}
             )
-            HorizontalDivider(
-                modifier = Modifier
-                    .padding(20.dp)
-                    .width(250.dp), thickness = Thickness, color =
-                    Color.Red
-            )
-            Row {
-                jenisK.forEach { item ->
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        RadioButton(
-                            selected = false,
-                            onClick = {item}
-                        )
-                        Text(text = item)
-                    }
-                }
-            }
+
             HorizontalDivider(
                 modifier = Modifier
                     .padding(20.dp)
@@ -85,20 +68,44 @@ fun FormIsian(
                 thickness = 1.dp,
                 color = Color.Red
             )
+
+            Row {
+                jenisK.forEach { item ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = false,
+                            onClick = { }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
             OutlinedTextField(
                 value = "",
                 singleLine = true,
-                modifier = Modifier
-                    .width(250.dp),
+                modifier = Modifier.width(250.dp),
                 label = { Text(text = "Alamat") },
-                onValueChange = {},
+                onValueChange = {}
             )
+
             Spacer(modifier = Modifier.height(30.dp))
+
             Button(
-                modifier = Modifier.fillMaxWidth(1f),
-                onClick = OnSubmitBtnClick
-            ){
-                Text(stringResource(id = R.string.submit))
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onSubmitBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.submit))
             }
         }
     }
