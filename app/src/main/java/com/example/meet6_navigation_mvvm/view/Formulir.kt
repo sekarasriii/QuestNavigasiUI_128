688978e3842ced2da48b219655912a64a32f911d
sekarasriii <sekareeesh@gmail.com>
2025-11-06 15:38:09 +0700

    menambahkan judul (home) di dalam top app bar

--- app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/view/Formulir.kt
@@ -4,11 +4,19 @@ import android.R
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import android.R.attr.title
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormIsian(
     jenisK:List<R.string> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ) {
-    Scaffold(modifier = Modifier)
+    Scaffold(modifier = Modifier,
+        topBar = {
+            title = {Text(text = stringResource(id= R.string.home),
+                color = Color.White)},
+        }
 }
