23897875b40b88ae3059ca1b8bf3f107e207365d
sekarasriii <sekareeesh@gmail.com>
2025-11-06 16:27:48 +0700

    menambahkan setup scaffold pada seluruh tampilan layar

--- app/src/main/java/com/example/meet6_navigation_mvvm/view/TampilData.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/view/TampilData.kt
@@ -2,9 +2,37 @@ package com.example.meet6_navigation_mvvm.view
 
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()->Unit
-){}
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = "ALAMAT"), "Yogyakarta")
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id=R.string.tampil),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource
+                    (id=R.color.teal_700)
+                )
+            )
+        })
+}
