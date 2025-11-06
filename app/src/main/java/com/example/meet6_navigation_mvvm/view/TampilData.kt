b4f12e082daf8431acde5eb09ae04f628841abfe
sekarasriii <sekareeesh@gmail.com>
2025-11-06 16:15:58 +0700

    menambahkan anotasi @OptIn(ExperimentalMaterial3Api::class)

--- app/src/main/java/com/example/meet6_navigation_mvvm/view/TampilData.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/view/TampilData.kt
@@ -1,2 +1,7 @@
 package com.example.meet6_navigation_mvvm.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
