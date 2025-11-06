7200b71bed56477dff21d07a307947ef63b7e1cc
sekarasriii <sekareeesh@gmail.com>
2025-11-06 17:02:49 +0700

    merapihkan code dan membenarkan error

--- app/src/main/java/com/example/meet6_navigation_mvvm/Navigasi.kt
+++ app/src/main/java/com/example/meet6_navigation_mvvm/Navigasi.kt
@@ -1,12 +1,17 @@
 package com.example.meet6_navigation_mvvm
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavControlle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.meet6_navigation_mvvm.view.FormIsian
+import com.example.meet6_navigation_mvvm.view.TampilData
 
-enum class Navigasi{
+enum class Navigasi {
     Formulir,
     Detail
 }
@@ -14,34 +19,39 @@ enum class Navigasi{
 @Composable
 fun DataApp(
     navController: NavHostController = rememberNavController(),
-    modifier: Modifier
-){
-    Scaffold { isiRuang->
-        navController = navController,
-        startDestination = Navigasi.Formulir.name,
-
-        modifier = Modifier.padding(isiRuang)){
-            composable(route = Navigasi.Formulir.name){
+    modifier: Modifier = Modifier
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
                 FormIsian(
-                    //pilihanJK = JenisK.map{ id -> konteks.resources.getString(id) }
-                    OnSubmitBtnClick = {
+                    // pilihanJK = JenisK.map { id -> konteks.resources.getString(id) }
+                    onSubmitBtnClick = {
                         navController.navigate(Navigasi.Detail.name)
                     }
                 )
             }
-            composable(route = Navigasi.Detail.name){
+
+            composable(route = Navigasi.Detail.name) {
                 TampilData(
                     onBackBtnClick = {
                         cancelAndBackToFormulir(navController)
                     }
                 )
             }
-    }}
+        }
     }
 }
+
 private fun cancelAndBackToFormulir(
     navController: NavHostController
-){
-    navController.popBackStack(Navigasi.Formulir.name,
-        inclusive = false)
+) {
+    navController.popBackStack(
+        route = Navigasi.Formulir.name,
+        inclusive = false
+    )
 }
